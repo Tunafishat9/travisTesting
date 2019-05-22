0a8fce9570c270015cf8c92f42432e2d0343d358
Tunafishat9 <u16099860@tuks.co.za>
2019-05-23 01:22:54 +0200

    commit changes made to the main file

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("testing");
+        System.out.println("somethign else ");
+        System.out.println("here is somemore things");
     }
 }
